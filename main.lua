735a414f8eb86c6d4c9d1e8483323311d2900cad
Finnbar Keating <finnjkeating@aol.com>
2015-02-02 17:16:05 +0000

    OK, it's now become a simple buzzer app
    It buzzes when you tap it! Yay science!

--- main.lua
+++ main.lua
@@ -1,1 +1,11 @@
-require "requirer"
+bg = love.graphics.newImage("oclogo.png")
+sound = love.audio.newSource("sound.mp3")
+
+function love.draw()
+	love.graphics.draw(bg,0,0)
+end
+
+function love.mousepressed()
+	love.audio.stop()
+	love.audio.play(sound)
+end
